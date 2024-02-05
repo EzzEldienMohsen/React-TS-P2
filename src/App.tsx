7188c52bafcfb77859fee1a96e63fef78101b745
Ezz dghedy <ezzmohsend@gmail.com>
2024-02-05 22:28:59 +0200

    Input with ref incase of using useRef()

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import Button from './components/Button';
-import Container from './components/Container';
+import React from 'react';
+
+import Input from './components/Input';
 
 function App() {
+  const input = React.useRef<HTMLInputElement>(null);
   return (
     <main>
-      <Container as={Button}>Click ME </Container>
+      <Input name="yourName" text="insert your name" type="text" ref={input} />
     </main>
   );
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,13 +4,15 @@ type Input = {
   name: string;
   text: string;
 } & React.ComponentPropsWithoutRef<'input'>;
-const Input: React.FC<Input> = ({ name, text, ...props }) => {
-  return (
-    <p>
-      <label htmlFor={name}>{text}</label>
-      <input id={name} name={name} {...props} />
-    </p>
-  );
-};
+const Input = React.forwardRef<HTMLInputElement, Input>(
+  ({ name, text, ...props }, ref) => {
+    return (
+      <p>
+        <label htmlFor={name}>{text}</label>
+        <input id={name} name={name} {...props} ref={ref} />
+      </p>
+    );
+  }
+);
 
 export default Input;
