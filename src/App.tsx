2025dd179f361bd1a8c76f26f1f9b40ef858c415
Ezz dghedy <ezzmohsend@gmail.com>
2024-02-06 00:18:23 +0200

    creating Form functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,19 @@ import Form from './components/Form';
 import Button from './components/Button';
 
 function App() {
+  function handleSave(data: unknown): void {
+    const extractedData = data as {
+      name: string;
+      email: string;
+      password: string;
+      age: string;
+    };
+    console.log(extractedData);
+  }
   return (
     <main>
-      <Form>
-        <Input name="yourName" text="insert your name" type="text" />
+      <Form onSave={handleSave}>
+        <Input name="name" text="insert your name" type="text" />
         <Input name="email" text="insert your email" type="email" />
         <Input name="password" text="insert your password" type="password" />
         <Input name="age" text="insert your age" type="number" />
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
-type FormProps = React.ComponentPropsWithoutRef<'form'>;
+type FormProps = React.ComponentPropsWithoutRef<'form'> & {
+  onSave: (value: unknown) => void;
+};
 
-const Form: React.FC<FormProps> = (props) => {
-  return <form {...props}>{props.children}</form>;
+const Form: React.FC<FormProps> = ({ onSave, children, ...props }) => {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData);
+    onSave(data);
+    e.currentTarget.reset();
+  }
+  return (
+    <form onSubmit={handleSubmit} {...props}>
+      {children}
+    </form>
+  );
 };
 
 export default Form;
