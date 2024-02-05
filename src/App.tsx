472041975a7082bb0d6e155cc96b58be0c0e4a89
Ezz dghedy <ezzmohsend@gmail.com>
2024-02-06 01:02:44 +0200

    useImparativeHandle

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Input from './components/Input';
-import Form from './components/Form';
+import Form, { type FormHandle } from './components/Form';
 import Button from './components/Button';
+import React from 'react';
 
 function App() {
+  const ref = React.useRef<FormHandle>(null);
   function handleSave(data: unknown): void {
     const extractedData = data as {
       name: string;
@@ -11,10 +13,11 @@ function App() {
       age: string;
     };
     console.log(extractedData);
+    ref.current?.clear();
   }
   return (
     <main>
-      <Form onSave={handleSave}>
+      <Form onSave={handleSave} ref={ref}>
         <Input name="name" text="insert your name" type="text" />
         <Input name="email" text="insert your email" type="email" />
         <Input name="password" text="insert your password" type="password" />
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,20 +2,33 @@ import React from 'react';
 type FormProps = React.ComponentPropsWithoutRef<'form'> & {
   onSave: (value: unknown) => void;
 };
-
-const Form: React.FC<FormProps> = ({ onSave, children, ...props }) => {
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const data = Object.fromEntries(formData);
-    onSave(data);
-    e.currentTarget.reset();
-  }
-  return (
-    <form onSubmit={handleSubmit} {...props}>
-      {children}
-    </form>
-  );
+export type FormHandle = {
+  clear: () => void;
 };
+const Form = React.forwardRef<FormHandle, FormProps>(
+  ({ onSave, children, ...props }, ref) => {
+    const form = React.useRef<HTMLFormElement>(null);
+    React.useImperativeHandle(ref, () => {
+      return {
+        clear() {
+          form.current?.reset();
+        },
+      };
+    });
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
+      e.preventDefault();
+      const formData = new FormData(e.currentTarget);
+      const data = Object.fromEntries(formData);
+      onSave(data);
+      // e.currentTarget.reset();
+      // form.current?.reset();
+    }
+    return (
+      <form onSubmit={handleSubmit} {...props} ref={form}>
+        {children}
+      </form>
+    );
+  }
+);
 
 export default Form;
